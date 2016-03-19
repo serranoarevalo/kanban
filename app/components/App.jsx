c2f65d02b6fa0f5598f73416276b836519303b76
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-03-19 16:41:12 +0700

    Webpack set up, data rendered with hardcoded model data

---
+++ app/components/App.jsx
@@ -0,0 +1,35 @@
+import uuid from 'node-uuid';
+import React from 'react';
+import Note from './Note.jsx';
+
+const notes = [
+	{
+		id: uuid.v4(),
+		task: 'Learn Webpack'
+	},
+	{
+		id: uuid.v4(),
+		task: 'Learn React'
+	},
+	{
+		id: uuid.v4(),
+		task: 'Do laundry'
+	}
+];
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ul>{notes.map(this.renderNote)}</ul>
+			</div>
+		);
+	}
+	renderNote(note) {
+		return (
+			<li key={note.id}>
+				<Note task={note.task} />
+			</li>
+		);
+	}
+}
